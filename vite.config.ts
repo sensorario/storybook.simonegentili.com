826a01a2bec434924ac36894f7591f9910f998ed
Simone Gentili <sensorario@gmail.com>
2026-05-24 07:42:48 +0200

    chore(release): fix release configuration

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,10 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
+  base: './',
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 2000,
     lib: {
       entry: path.resolve(dirname, 'src/index.ts'),
       name: 'SGComponents',
