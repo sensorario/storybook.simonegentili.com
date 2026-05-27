f09174c9145d8e8651566b37aedf8b709f9c9585
Simone Gentili <sensorario@gmail.com>
2026-05-27 22:05:31 +0200

    chore(footer): reduce links label

--- src/stories/SGFooter/SGFooter.tsx
+++ src/stories/SGFooter/SGFooter.tsx
@@ -1,10 +1,10 @@
 import { Footer } from '../Footer/Footer';
 
 const links = [
-  { label: 'guitar.simonegentili.com', href: 'https://guitar.simonegentili.com' },
-  { label: 'quadrato.simonegentili.com', href: 'https://quadrato.simonegentili.com' },
-  { label: 'gantt.simonegentili.com', href: 'https://gantt.simonegentili.com' },
-  { label: 'code2image.simonegentili.com', href: 'https://code2image.simonegentili.com' },
+  { label: 'guitar', href: 'https://guitar.simonegentili.com' },
+  { label: 'quadrato', href: 'https://quadrato.simonegentili.com' },
+  { label: 'gantt', href: 'https://gantt.simonegentili.com' },
+  { label: 'code2image', href: 'https://code2image.simonegentili.com' },
 ];
 
 export const SGFooter = () => {
