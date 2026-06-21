cd812caecaa0a93a55a8760a854914d85b2713e1
Simone Gentili <sensorario@gmail.com>
2026-06-21 22:59:35 +0200

    chore(footer): update links

--- src/components/SG/Footer/SGFooter.tsx
+++ src/components/SG/Footer/SGFooter.tsx
@@ -2,10 +2,10 @@ import { Footer } from '../../Footer/Footer';
 
 const links = [
   { label: 'guitar', href: 'https://guitar.simonegentili.com' },
+  { label: 'tome', href: 'https://tome.simonegentili.com' },
   { label: 'quadrato', href: 'https://quadrato.simonegentili.com' },
   { label: 'gantt', href: 'https://gantt.simonegentili.com' },
   { label: 'code2image', href: 'https://code2image.simonegentili.com' },
-  { label: 'tome', href: 'https://tome.simonegentili.com' },
 ];
 
 export const SGFooter = () => {
