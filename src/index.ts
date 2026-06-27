080b620d837f967321bc6f2a48656c951fbfa048
Simone Gentili <sensorario@gmail.com>
2026-06-27 03:01:48 +0200

    feat(LoginModal): add component

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './components/SG/Footer/SGFooter';
 export * from './components/Header/Header';
 export * from './components/SG/Header/SGHeader';
 export * from './components/Authenticator/Authenticator';
+export * from './components/LoginModal/LoginModal';
